a1cc5ba7cc12ca973a88c3bfa0fa606358efbd86
Ankit Kumar <ankit.official.dev@gmail.com>
2020-07-03 03:13:33 +0530

    fixed Async call

--- gui/js/behaviour.js
+++ gui/js/behaviour.js
@@ -1,4 +1,23 @@
 const remote = require('electron').remote;
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+ 
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+const uuid = require('uuid')
+ 
+// Initiallizing playlist in DataBase
+db.defaults({ PlayLists: [] })
+  .write()
+
+
+
+//gloabal variables to manage Playlists
+
+var toBeAddedPaths;
+
+// global variable ends
 
 function closewindow(){
     var window = remote.getCurrentWindow();
@@ -33,9 +52,31 @@ function volumedisptoggle(){
 
 // functional contents
 
-function addplaylist_browse(){
+async function addplaylist_browse(){
     const { dialog } = require('electron').remote;
-    dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] });
+    selections = dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'], filters: [
+        { name: 'Audio Files', extensions: ['mp3', 'ogg', 'MP3','OGG', 'wav', 'WAV'] },
+      ]
+     });
+    var resolvedSelections = Promise.resolve(selections);
+    toBeAddedPaths = await resolvedSelections.then((path)=>{
+        paths = path.filePaths;
+        totalFilesSelected = paths.length;
+        document.getElementById('noofsel').innerHTML = totalFilesSelected;
+        return paths;
+    })
+}
+
+function addPlaylistToDB() {
+    var playName = document.getElementById('playlistnametoadd').value;
+    readyList = {};
+    currentList = [];
+    for(x = 0; x < toBeAddedPaths.length; x++){
+        formattedPath = {'id':uuid.v4() , 'path':toBeAddedPaths[x]};
+        currentList.push(formattedPath);
+    }
+    readyList[playName] = currentList;
+    db.get('PlayLists').push(readyList).write();
 }
 
 
